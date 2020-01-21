6f7f833dcfd5b9cb7f318748b45d0ec8f039725e
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2020-01-21 09:17:44 +0530

    Create BirthdayCandle.cs

---
+++ BirthdayCandle.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] array) {
+
+
+        int length = array.Length;
+
+        int MaxValue = array.Max();
+        int MaxCount = 0;
+
+        //foreach loop to find the number of max elements in the array
+        foreach(int index in array)
+        {
+            if(MaxValue==index)
+                MaxCount ++;
+        }
+
+        return (MaxCount);
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = birthdayCakeCandles(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+/*
+
+Sample Input 0
+
+4
+3 2 1 3
+
+Sample Output 0
+
+2
+Explanation 0
+
+We have one candle of height , one candle of height , and two candles of height . Your niece only blows out the tallest candles, meaning the candles where . Because there are  such candles, we print  on a new line.
+
+Solved by SanthoshGoku
+HackerRank Problem
+
+*/
